a332c9a462468072b05458440f391668b82484f2
Adam Greenhall <adam.greenhall@gmail.com>
2011-09-27 15:42:42 -0700

    added solver option

--- minpower/tests/test.py
+++ minpower/tests/test.py
@@ -7,10 +7,10 @@ import os
 from minpower import solve,config
 from minpower.commonscripts import joindir,splitFilename
 
-def problem(test_name='uc'):
+def problem(test_name='uc',solver=config.optimization_solver):
     test_dir=splitFilename(__file__)[0]
     dir=joindir(test_dir,test_name)
-    if os.path.isdir(dir): solve.problem(dir)
+    if os.path.isdir(dir): solve.problem(dir,solver=solver)
     else:
         msg='"{}" is not a minpower test case.'.format(test_name) 
         raise OSError(msg)

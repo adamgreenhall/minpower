e5c352b3341709e0ca2b69ec4e571fe5249f70a3
Adam Greenhall <adam.greenhall@gmail.com>
2011-09-26 23:00:27 -0700

    added publish option

--- doc/build.py
+++ doc/build.py
@@ -1,8 +1,19 @@
-#!/usr/bin/env python
-import os, subprocess
+import os, subprocess, sys
 import glob
-#if not os.getcwd().endswith('doc'): os.chdir('./doc')
-os.system('make html')
-#os.system('make latexpdf')
-for f in glob.glob('*.log'): os.remove(f)
-for f in glob.glob('../*.pyc'): os.remove(f)
+
+def main(publish=False):
+    #if not os.getcwd().endswith('doc'): os.chdir('./doc')
+    os.system('make html')
+    #os.system('make latexpdf')
+    for f in glob.glob('*.log'): os.remove(f)
+    for f in glob.glob('../*.pyc'): os.remove(f)
+
+if __name__ == "__main__": 
+    ''' command line input'''
+    if len(sys.argv)==1: main()
+    elif len(sys.argv)==2: 
+        ifpub=sys.argv[1] == 'publish'
+        main(publish=ifpub)
+    else: 
+        raise OSError('build takes 0 or 1 arguments')
+

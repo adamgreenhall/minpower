f8cc3a0b48aa9ae3ca246a1448d8eadf14c323a8
Adam Greenhall <adam.greenhall@gmail.com>
2011-09-26 21:30:16 -0700

    added makefile, mathjax

--- doc/build.py
+++ doc/build.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os, subprocess
 import glob
-os.chdir('./doc')
+#if not os.getcwd().endswith('doc'): os.chdir('./doc')
 os.system('make html')
 #os.system('make latexpdf')
 for f in glob.glob('*.log'): os.remove(f)

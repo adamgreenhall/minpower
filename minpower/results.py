7ae8b8601898a5ff36f0fbd0dee040db5e40b474
Adam Greenhall <adam.greenhall@gmail.com>
2012-05-10 12:27:30 -0700

    move scenario snapshot over to optimization.py

--- minpower/results.py
+++ minpower/results.py
@@ -449,7 +449,6 @@ class Solution_Stochsatic(Solution):
     def __init__(self,power_system,times,datadir='.'):
         update_attributes(self,locals())
         self.scenarios=sorted(self.power_system._scenario_instances.keys())
-        self.power_system._scenario_tree.snapshotSolutionFromInstances(self.power_system._scenario_instances)
         
         self._get_problem_info()
         self._get_costs()

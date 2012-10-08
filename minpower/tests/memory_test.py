831e8689c14af5b7c904cc6a29945f4fc18a4bf0
Adam Greenhall <adam.greenhall@gmail.com>
2012-10-08 13:58:17 -0700

    memory tests that show leaking objects

--- minpower/tests/memory_test.py
+++ minpower/tests/memory_test.py
@@ -5,7 +5,38 @@ from test_utils import solve_problem,make_loads_times,make_cheap_gen,make_mid_ge
 from coopr import pyomo
 
 
-import objgraph
+import objgraph, inspect
+
+def get_counts(prefix=''):
+    test_types = [
+        'Var', 
+        'Piecewise',
+        'ScenarioTree',
+        'ScenarioTreeNode',
+        'Scenario',
+        '_SetContainer',
+        '_ConstraintArray',
+        ]
+    
+    # use objgraph to check if these pyomo objects still exist 
+    objects = {} 
+    test_counts = {}
+    for name in test_types:
+        objects[name] = objgraph.by_type( name )
+        test_counts[name] = len(objects[name])
+    
+    if True:
+        for name in test_types:
+            if test_counts[name]==0: continue
+            else: obj = objects[name][0]
+            fname = prefix+'-'+'objgraph-{}-'.format(name)
+            objgraph.show_refs( [obj], filename=fname+'refs.png', too_many=50)
+            objgraph.show_backrefs([obj], filename=fname+'backref.png')
+            objgraph.show_chain(
+                objgraph.find_backref_chain( obj, inspect.ismodule),
+                filename=fname+'chain.png'
+                )
+    return test_counts    
 
 mem = Tests()
 
@@ -22,19 +53,9 @@ def leak_on_reset():
     power_system, times = solve_problem(generators, **loads_times)
     # reset the model - no pyomo components should persist
     power_system.reset_model()
-    
-    test_types = dict(
-        variable  = pyomo.Var, 
-        piecewise = pyomo.Piecewise,
-        )
-    
-    # use objgraph to make sure they don't 
-    test_counts = {}
-    for name, kind in test_types.iteritems():
-        test_counts[name] = len(objgraph.by_type( kind ))
         
+    test_counts = get_counts('uc')    
     assert sum(test_counts.values()) == 0
-
     
 if __name__ == "__main__": 
     mem.run()

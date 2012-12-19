8defbf29eda51056e9a8c6d2bfdecc5408c26297
Adam Greenhall <adam.greenhall@gmail.com>
2012-12-18 16:51:49 -0800

    scaffold solve to make standalone and tables not the default

--- minpower/solve.py
+++ minpower/solve.py
@@ -12,8 +12,6 @@ import pdb
 from optimization import OptimizationError
 import get_data, powersystems, stochastic, results
 from config import user_config
-from standalone import (store_state, load_state, store_times, init_store,
-    get_storage, repack_storage)
 from commonscripts import joindir, StreamToLogger, set_trace
 
 def _get_store_filename():
@@ -23,7 +21,7 @@ def _get_store_filename():
 
     user_config.store_filename = joindir(user_config.directory, fnm)
 
-def solve_multistage(power_system, times, scenario_tree, data):
+def solve_multistage_standalone(power_system, times, scenario_tree, data):
     _get_store_filename()
 
     stage_times = times.subdivide(
@@ -131,8 +129,15 @@ def solve_problem(datadir='.',
     if times.spanhrs <= user_config.hours_commitment + user_config.hours_overlap:
         solution, instance = create_solve_problem(power_system, times, scenario_tree)
     else: #split into multiple stages and solve
-        stage_solutions, stage_times = solve_multistage(
-            power_system, times, scenario_tree, data)
+        if user_config.standalone:
+            from standalone import (store_state, load_state, store_times,
+                init_store, get_storage, repack_storage)
+
+            stage_solutions, stage_times = solve_multistage_standalone(
+                power_system, times, scenario_tree, data)
+        else:
+            stage_solutions, stage_times = solve_multistage(
+                power_system, times, scenario_tree, data)
         solution = results.make_multistage_solution(
             power_system, stage_times, stage_solutions)
 
@@ -148,6 +153,23 @@ def solve_problem(datadir='.',
     # if solution_file: solution.save(solution_file)
     logging.info('total time: {}s'.format(timer.time()-start_time))
     return solution
+
+
+def solve_multistage(power_system, times, scenario_tree=None, data=None):
+    stage_times = times.subdivide(
+        user_config.hours_commitment, user_config.hours_overlap)
+
+    stage_solutions = []
+
+    for stg,t_stage in enumerate(stage_times):
+        logging.info('Stage starting at {}'.format(t_stage.Start.date()))
+        # set initial state
+        # solve
+        # set final state
+        # add to stage solutions
+
+    return stage_solutions, stage_times
+
 
 def create_solve_problem(power_system, times, scenario_tree=None,
     multistage=False, stage_number=None, rerun=False):
@@ -179,13 +201,13 @@ def create_problem(power_system, times, scenario_tree=None,
     logging.debug('created constraints')
 
     if scenario_tree is not None and sum(scenario_tree.shape)>0 and not rerun:
-        if multistage: # multiple time stages
-            gen = power_system.get_generator_with_scenarios()
-            tree = stochastic.construct_simple_scenario_tree(
-                gen.scenario_values[times.Start]['probability'].values.tolist(),
-                time_stage=stage_number)
-            logging.debug('constructed tree for stage %i'%stage_number)
-        else: tree = scenario_tree
+        gen = power_system.get_generator_with_scenarios()
+        tree = stochastic.construct_simple_scenario_tree(
+            gen.scenario_values[times.Start]['probability'].values.tolist(),
+            time_stage=stage_number)
+
+        logging.debug('constructed tree for stage %i'%stage_number)
+
         stochastic.define_stage_variables(tree, power_system, times)
         power_system = stochastic.create_problem_with_scenarios(
             power_system, times, tree,
@@ -274,6 +296,8 @@ def main():
         help="""allow faststart units which are off to be
                 started up during resolve""")
 
+    parser.add_argument('--standalone', '-m', action="store_true", default=False,
+        help='Make each multi-day commitment its own subprocess (helps with memory issues).')
     parser.add_argument('--output_prefix','-p', action="store_true",
         default=user_config.output_prefix,
         help = 'Prefix all results files with the process id (for a record of simulataneous solves)')

79905ac1f6f46e01eadb34f456ea11405c858b23
Adam Greenhall <adam.greenhall@gmail.com>
2013-01-16 11:36:02 -0800

    setup a full dependencies travis test

---
+++ .travis_install.sh
@@ -0,0 +1,8 @@
+sudo apt-get install python-numpy glpk
+pip install --use-mirrors .
+
+if [ x"$FULL_DEPS" == x"true" ]; then
+    echo "Installing FULL_DEPS"
+    sudo apt-get install libhdf5-serial-dev
+    pip install numexpr tables matplotlib
+fi

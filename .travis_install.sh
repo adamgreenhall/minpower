223410f5fd6fd6f45904386ffce6ee982fac281f
Adam Greenhall <adam.greenhall@gmail.com>
2013-01-16 18:18:40 -0800

    travis install: cython is a tables requirement

--- .travis_install.sh
+++ .travis_install.sh
@@ -4,6 +4,6 @@ pip install --use-mirrors .
 if [ x"$FULL_DEPS" == x"true" ]; then
     echo "Installing FULL_DEPS"
     sudo apt-get install libhdf5-serial-dev
-    pip install numexpr
+    pip install numexpr cython
     pip install tables matplotlib
 fi

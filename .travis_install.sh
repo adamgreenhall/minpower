788c29a459e62e5662103ef61fcf224a7b22b3cf
Adam Greenhall <agreenhall@lyft.com>
2015-05-09 17:49:27 -0700

    use conda for installing on travisci

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,10 +1,8 @@
 sudo apt-get install glpk
-pip install numpy==1.9.1
-pip install --use-mirrors .
+conda install --yes python=$TRAVIS_PYTHON_VERSION --file requirements.conda.txt
+pip install .
 
 if [ x"$FULL_DEPS" == x"true" ]; then
     echo "Installing FULL_DEPS"
-    sudo apt-get install libhdf5-serial-dev
-    pip install numexpr cython
-    pip install tables matplotlib
+    conda install --yes python=$TRAVIS_PYTHON_VERSION --file requirements.conda.full_deps.txt
 fi

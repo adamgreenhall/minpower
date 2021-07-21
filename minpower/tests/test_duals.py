ca2a5b408e991084baaa7ae9ebad03e3c4d015c5
Adam Greenhall <adam.greenhall@gmail.com>
2021-07-21 12:06:07 -0700

    test duals problem

--- minpower/tests/test_duals.py
+++ minpower/tests/test_duals.py
@@ -5,20 +5,47 @@ from .test_utils import istest
 
 @istest
 def test_pyomo_duals():
-    # Create a solver
-    opt = SolverFactory("cbc")
-
     # create a model
     model = pyo.ConcreteModel()
+    # Create a 'dual' suffix component on the instance
+    # so the solver plugin will know which suffixes to collect
+    model.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
     model.x = pyo.Var([1, 2], domain=pyo.NonNegativeReals)
     model.OBJ = pyo.Objective(expr=2 * model.x[1] + 3 * model.x[2])
     model.Constraint1 = pyo.Constraint(expr=3 * model.x[1] + 4 * model.x[2] >= 1)
 
-    # Create a 'dual' suffix component on the instance
-    # so the solver plugin will know which suffixes to collect
-    model.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
-
-    opt.solve(model)  # also load results to model
+    opt = SolverFactory("cbc")
+    opt.solve(model)
 
     # make sure the dual is non-zero (constraint is binding)
     assert model.dual[model.Constraint1] != 0
+
+
+@istest
+def test_duals_mip():
+    model = pyo.ConcreteModel()
+    model.dual = pyo.Suffix(direction=pyo.Suffix.IMPORT)
+
+    model.x = pyo.Var([1, 2], domain=pyo.Boolean)
+    model.y = pyo.Var([1, 2], domain=pyo.NonNegativeReals)
+    # this problem is basically just two generators:
+    # gen1 is cheap, but limited output
+    # gen2 is more expensive
+    model.OBJ = pyo.Objective(expr=2 * model.y[1] + 3 * model.y[2])
+    model.Constraint1 = pyo.Constraint(expr=model.y[1] <= 50 * model.x[1])
+    model.Constraint2 = pyo.Constraint(expr=model.y[2] <= 80 * model.x[2])
+    model.Constraint3 = pyo.Constraint(expr=model.y[1] + model.y[2] == 100)
+
+    opt = SolverFactory("cbc")
+    opt.solve(model)
+
+    # make sure the solution is what we'd expect (both gen need to run)
+    assert model.x[1].value == True
+    assert model.x[2].value == True
+
+    # fix binary variables
+    model.x[1].fixed = True
+    model.x[2].fixed = True
+    opt.solve(model)
+    # make sure the dual is non-zero (constraint is binding)
+    assert model.dual[model.Constraint3] != 0
